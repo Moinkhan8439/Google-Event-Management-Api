1e7002f7b3e9b15b5a33e3a93c87a5b1403b81b8
Moin <moinkhan8439@gmail.com>
2021-03-22 11:38:03 +0530

    adding Procfile and extras

--- Todogoogle/settings.py
+++ Todogoogle/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q=e(&a750oo0y&a6j9)**4e&ogl&0#vp5q6x+zm29yuul_*w8g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['calendar-assistance.herokuapp.com/']
 
 
 # Application definition
@@ -149,5 +149,5 @@ SOCIALACCOUNT_PROVIDERS = {
 SITE_ID = 1
 
 
-LOGIN_REDIRECT_URL = '/welcome'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
